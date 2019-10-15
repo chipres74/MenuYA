660039daf5c47b9634a914ef4802921b124359bd
Alumno <Alumno@PC25>
2019-10-15 13:20:05 -0500

    chipresito

---
+++ ProgramaMenu/ProgramaMenu/ProgramaMenu.cpp
@@ -0,0 +1,116 @@
+// ProgramaMenu.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <string>
+#include <iostream>
+#include <cmath>
+#include<cstdlib>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+void pausa();
+
+int main()
+{
+	bool bandera = false;
+	char tecla;
+	do
+	{
+		system("cls");
+		cin.clear();
+		cout << "Conversiones" << endl;
+		cout << "-----------" << endl << endl;
+		cout << "\t1 .- Binario-Decimal" << endl;
+		cout << "\t2 .- Decimal-Binario" << endl;
+		cout << "\t3 .- Binario-Decimal" << endl;
+		cout << "\t4 .- Binario-Hexadecimal" << endl;
+		cout << "\t5 .- Texto-Binario" << endl;
+		cout << "\t6 .- Salir" << endl;
+		cout << "Elige una opcion: ";
+		cin >> tecla;
+		switch (tecla)
+		{
+		case '1':
+			system("cls");
+			cout << "Has elejido Binario-Decimal. \n";
+			{
+				int exp, digito;
+				double binario, decimal;
+				cout << "Introduce el numero binario: ";
+				cin >> binario;
+				exp = 0;
+				decimal = 0;
+				while (((int)(binario / 10)) != 0)
+				{
+					digito = (int)binario % 10;
+					decimal = decimal + digito * pow(2.0, exp);
+					exp++;
+					binario = (int)(binario / 10);
+				}
+				decimal = decimal + binario * pow(2.0, exp);
+				cout << endl << "El Decimal es: " << decimal << endl;
+			}
+			pausa();
+			break;
+		case '2':
+			system("cls");
+			cout << "Has elejido Decimal-Binario. \n";
+			{
+				int numero, exp, digito;
+				double binario;
+				cout << "Introduce numero: ";
+				cin >> numero;
+				exp = 0;
+				binario = 0;
+				while (numero / 2 != 0)
+				{
+					digito = numero % 2;
+					binario = binario + digito * pow(10.0, exp);
+					exp++;
+					numero = numero / 2;
+				}
+				binario = binario + numero * pow(10.0, exp);
+				cout << fixed << setprecision(0);
+				cout << "binario: " << binario << endl;
+			}
+			pausa();
+			break;
+		case '3':
+			system("cls");
+			cout << "Has elejido Binario-Octal. \n";
+
+			pausa();
+			break;
+		case '4':
+			system("cls");
+			cout << "Has elejido Binario-Hexadecimal. \n";
+			pausa();
+			break;
+		case '5':
+			system("cls");
+			cout << "Has elejido Texto-Binario. \n";
+			pausa();
+			break;
+		case '6':
+			bandera = true;
+			// exit(1);
+			break;
+		default:
+			system("cls");
+			cout << "Opcion no valida. \a\n";
+			pausa();
+			break;
+
+		}
+
+	} while (bandera != true);
+	return 0;
+
+}
+//Desarrollamos la función
+void pausa()
+{
+	cout << "Pulsa una tecla para continuar... ";
+	getwchar();
+	getwchar();
+}
