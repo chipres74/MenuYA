21cf99320053769092dcddecae38e2c8019686e0
Alumno <Alumno@PC25>
2019-10-17 12:30:21 -0500

    Xime.1

--- ProgramaMenu/ProgramaMenu/ProgramaMenu.cpp
+++ ProgramaMenu/ProgramaMenu/ProgramaMenu.cpp
@@ -1,14 +1,104 @@
-// ProgramaMenu.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <string>
 #include <iostream>
 #include <cmath>
 #include<cstdlib>
 #include <iomanip>
 #include <cmath>
+#include <stdio.h>
 using namespace std;
 void pausa();
+
+#define DEFAULT0 "\x1b[0m" //TEXTO
+#define DEFAULT1 "\x1b[39m" // COLOR
+#define DEFAULTF "\x1b[49m" // FONDO
+
+//== = Colores == = 
+//== = Texto == = 
+//-Rojo 
+#define RED "\x1b[31m" 
+//- Verde 
+#define GREEN "\x1b[32m" 
+//- Negro
+#define BLACK "\x1b[30m" 
+//- Azul 
+#define BLUE "\x1b[34m" 
+//- Amarillo 
+#define YELLOW "\x1b[33m" 
+//- Magenta 
+#define MAGENTA "\x1b[35m" 
+//- Cyan
+#define CYAN "\x1b[36m" 
+//- Blanco 
+#define WHITE "\x1b[37m" 
+//- Gris Claro
+#define LIGHT_GRAY "\x1b[90m" 
+//- Rojo Claro 
+#define LIGHT_RED "\x1b[91m" 
+//- Verde Claro 
+#define LIGHT_GREEN "\x1b[92m" 
+//- Amarillo Claro
+#define LIGHT_YELLOW "\x1b[93m" 
+//- Azul Claro 
+#define LIGHT_BLUE "\x1b[94m" 
+//- Magenta Claro 
+#define LIGHT_MAGENTA "\x1b[95m" 
+//- Cyan Claro 
+#define LIGHT_CYAN "\x1b[96m"
+//- Blanco Claro 
+#define LIGHT_WHITE "\x1b[97m" 
+
+//== = Modificadores == = 
+//== = Texto == = 
+//-Dim(No identificado aun) 
+#define DIM "\x1b[2m" 
+//- Reverse(Si el fondo es negro, el texto sera blanco y al contrario) 
+#define REVERSE "\x1b[7m"
+//- Hidden(No identificado aun) 
+#define HIDDEN "\x1b[8b" 
+//- Bold(ON / OFF) (No identificado aun) 
+#define BOLD_ON "\x1b[1m" 
+#define BOLD_OFF "\x1b[21m" 
+//- Underline(ON / OFF) (Subraya el texto) 
+#define UNDERLINE_ON "\x1b[4m" 
+#define UNDERLINE_OFF "\x1b[24m" 
+//- Blink(ON / OFF) (No identificado aun) 
+#define BLINK_ON "\x1b[5m" 
+#define BLINK_OFF "\x1b[25m" 
+
+//== = Fondos == = 
+//== = Texto == = 
+//-Negro 
+#define BLACKF "\x1b[40m" 
+//- Rojo 
+#define REDF "\x1b[41m" 
+//- Verde 
+#define GREENF "\x1b[42m" 
+//- Amarillo 
+#define YELLOWF "\x1b[43m" 
+//- Azul 
+#define BLUEF "\x1b[44m" 
+//- Magenta
+#define MAGENTAF "\x1b[45m" 
+//- Cyan
+#define CYANF "\x1b[46m" 
+//- Blanco
+#define WHITEF "\x1b[47m"
+//- Gris Claro
+#define LIGHT_GRAYF "\x1b[100m" 
+//- Rojo Claro
+#define LIGHT_REDF "\x1b[101m" 
+//- Verde Claro
+#define LIGHT_GREENF "\x1b[102m" 
+//- Amarillo Claro
+#define LIGHT_YELLOWF "\x1b[103m" 
+//- Azul Claro 
+#define LIGHT_BLUEF "\x1b[104m" 
+//- Magenta Claro
+#define LIGHT_MAGENTAF "\x1b[105m" 
+//- Cyan Claro 
+#define LIGHT_CYANF "\x1b[106m" 
+//- Blanco Claro
+#define LIGHT_WHITEF "\x1b[107m" 
 
 int main()
 {
@@ -18,14 +108,23 @@ int main()
 	{
 		system("cls");
 		cin.clear();
-		cout << "Conversiones" << endl;
-		cout << "-----------" << endl << endl;
-		cout << "\t1 .- Binario-Decimal" << endl;
-		cout << "\t2 .- Decimal-Binario" << endl;
-		cout << "\t3 .- Binario-Octal" << endl;
-		cout << "\t4 .- Binario-Hexadecimal" << endl;
-		cout << "\t5 .- Texto-Binario" << endl;
-		cout << "\t6 .- Salir" << endl;
+		system("color 7F");
+		cout << YELLOWF << BLUE << "----------------------------------" DEFAULT0 << endl;
+		cout << YELLOWF << BLUE << "||    C O N V E R S I O N E S   ||" DEFAULT0 << endl;
+		cout << YELLOWF << BLUE << "----------------------------------" DEFAULT0 << endl;
+		cout << YELLOWF << BLUE << "||                              ||" DEFAULT0 << endl;
+		cout << MAGENTAF << WHITE << "||     \t1.- Binario-Decimal     ||" DEFAULT0 << endl;
+		cout << YELLOWF << BLUE << "----------------------------------" DEFAULT0 << endl;
+		cout << MAGENTAF << WHITE << "||     \t2.- Decimal-Binario     ||" DEFAULT0 << endl;
+		cout << YELLOWF << BLUE << "----------------------------------" DEFAULT0 << endl;
+		cout << MAGENTAF << WHITE << "||     \t3.- Binario-Octal       ||" DEFAULT0 << endl;
+		cout << YELLOWF << BLUE << "----------------------------------" DEFAULT0 << endl;
+		cout << MAGENTAF << WHITE << "||     \t4.- Binario-Hexadecimal ||" DEFAULT0 << endl;
+		cout << YELLOWF << BLUE << "----------------------------------" DEFAULT0 << endl;
+		cout << MAGENTAF << WHITE << "||     \t5.- Texto-Binario       ||" DEFAULT0 << endl;
+		cout << YELLOWF << BLUE << "----------------------------------" DEFAULT0 << endl;
+		cout << "||    \t6.- Salir               ||" << endl;
+		cout << YELLOWF << BLUE << "----------------------------------" DEFAULT0 << endl;
 		cout << "Elige una opcion: ";
 		cin >> tecla;
 		switch (tecla)
@@ -122,7 +221,7 @@ int main()
 				for (i = 1; i <= num; i++) {
 					a = a + res[i - 1];
 				}
-				cout << a << endl;
+				cout <<"El resultado es: " << a << endl;
 			}
 			pausa();
 			break;
